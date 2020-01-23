cd09d8a38c7303eab1544f806db52105e8f91f2d
Christobal <geekarist@users.noreply.github.com>
2020-01-23 08:30:20 +0100

    Complete step 3

--- src/cljs/reagent_phonecat/core.cljs
+++ src/cljs/reagent_phonecat/core.cljs
@@ -1,5 +1,6 @@
 (ns reagent-phonecat.core
-  (:require [reagent.core :as rg])
+  (:require [reagent.core :as rg]
+            [clojure.string :as str])
   )
 
 (enable-console-print!)
@@ -8,6 +9,18 @@
 Try and call this function from the ClojureScript REPL."
   [& [name]]
   (print "Hello," (or name "World") "!"))
+
+(defn matches-search? "Determines if a phone item matches a text query."
+  [search data]
+  (let [qp (-> search
+               (or "")
+               str/lower-case
+               re-pattern)]
+    (->> (vals data)
+         (filter string?)
+         (map str/lower-case)
+         (some #(re-find qp %))
+         )))
 
 ;; --------------------------------------------
 ;; Application data
@@ -24,25 +37,57 @@ Try and call this function from the ClojureScript REPL."
   <phones-list>
   <phone-item>)
 
-(defn <phones-list> "An unordered list of phones"
-  [phones-list]
-  [:div.container-fluid
-   [:ul
-    (for [phone phones-list]
-      ^{:key (:name phone)} [<phone-item> phone]
-      )]])
+(defonce state (rg/atom {:phones hardcoded-phones-data
+                         :search ""}))
+
+(defn <phones-list>
+  "An unordered list of phones"
+  [phones-list search]
+  (let [phones-list-filtered (->> phones-list (filter #(matches-search? search %)))]
+    [:div.container-fluid
+     [:ul
+      (for [phone phones-list-filtered]
+        ^{:key (:name phone)} [<phone-item> phone]
+        )]]))
 
 (defn <phone-item> "An phone item component"
-  [{:keys [name description] :as phone}]
+  [{:keys [name description] :as _}]
   [:li.phone-item
    [:span name]
    [:p description]])
 
+(defn <search-component> "The search input box" [search]
+  [:span "Search: "
+   [:input {:type      "text"
+            :value     search
+            :on-change (fn [e] (swap! state update-search (-> e .-target .-value)))}]])
+
+(defn <top-component> []
+  (let [{:keys [phones search]} @state]
+    [:div.container-fluid
+     [:div.row
+      [:div.col-md-2 [<search-component> search]]
+      [:div.col-md-8 [<phones-list> phones search]]]]))
+
+(defn update-search [state new-search]
+  (assoc state :search new-search))
+
 (defn mount-root "Creates the application view and injects ('mounts') it into the root element."
   []
   (rg/render
-    [<phones-list> hardcoded-phones-data]
+    [<top-component>]
     (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
+
+(comment
+  (in-ns 'reagent-phonecat.core)
+  (matches-search? "yo2" {:yo "yo"})
+  (if (matches-search? "yx.*" {:yo "yo" :yo2 "yo2"}) "OK" "Aww")
+  (matches-search? "todo" {:name "todo" :description "todo"})
+  (<phones-list> [{:name "Nexus S" :description "Fast just got faster with Nexus S"}] "faster")
+  (<phone-item> {:name "Nexus S" :description "Fast just got faster with Nexus S"})
+  (swap! state assoc :search "fast")
+  (swap! state assoc :search "xoom")
+  )
